5bd5cd44e266ff940cfb0134d9ecec34e66c42d9
dhariyani8 <dhariyani8@gmail.com>
2026-04-09 19:29:08 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import MenuSection from "./components/MenuSection";
+import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
+
 export default function App() {
-  return <h1 style={{ color: "black" }}>Hello World</h1>;
+  return (
+    <div style={{ background: "white", minHeight: "100vh", color: "black" }}>
+      <Navbar />
+
+      <main>
+        <Hero />
+        <MenuSection />
+        <Gallery />
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
